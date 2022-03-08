7b7934b8b93c1c863e8fa410184008c15ab8f088
NathanDDarmawan <92316923+NathanDDarmawan@users.noreply.github.com>
2022-03-08 20:56:18 +0700

    Create Q11.java

---
+++ week 3 forum/Q11.java
@@ -0,0 +1,12 @@
+public class Q11
+{
+	public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        int temp; // placeholder value
+        
+        temp = x;
+        x = y;
+        y = temp;
+	}
+}
